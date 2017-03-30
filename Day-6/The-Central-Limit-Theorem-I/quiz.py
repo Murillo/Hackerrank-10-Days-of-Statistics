1ada12742855b232a2fb06ecfe7bc6c0daf6e936
Murillo <msgrubler@gmail.com>
2017-03-29 21:54:58 -0300

    Start quiz about Central Limit Theorem

---
+++ Day-6/The-Central-Limit-Theorem-I/quiz.py
@@ -0,0 +1,6 @@
+# Question: A large elevator can transport a maximum of 9800 pounds. Suppose a
+# load of cargo containing 49 boxes must be transported via the elevator.
+# The box weight of this type of cargo follows a distribution with a mean
+# of u = 205 pounds and a standard deviation of a = 15 pounds. Based on this
+# information, what is the probability that all 49 boxes can be safely loaded
+# into the freight elevator and transported?
