ff6375e837a2a37483a1b9fffa3178921913e5c7
Murillo <msgrubler@gmail.com>
2017-03-30 23:09:27 -0300

    Change first quiz about CLT

--- Day-6/The-Central-Limit-Theorem-I/quiz.py
+++ Day-6/The-Central-Limit-Theorem-I/quiz.py
@@ -4,3 +4,22 @@
 # of u = 205 pounds and a standard deviation of a = 15 pounds. Based on this
 # information, what is the probability that all 49 boxes can be safely loaded
 # into the freight elevator and transported?
+
+# Import library
+import math
+
+# Define functions
+def cumulative(mean, std, value):
+    return 0.5 * (1 + math.erf((value - mean) / (std * (2 ** 0.5))))
+
+# Set data
+max_weight = float(input())
+n = float(input())
+mean = float(input())
+std = float(input())
+
+new_mean = mean * n
+new_std = math.sqrt(n) * std
+
+# Gets the result and show on the screen
+print (round(cumulative(new_mean, new_std, max_weight),4))
