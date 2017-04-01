53ca52ac0b320011c57d33d3c65afca03ad5a91b
Murillo <msgrubler@gmail.com>
2017-04-01 19:56:15 -0300

    Quiz about Spearman's Rank Correlation

---
+++ Day-7/Spearman-Rank-Correlation-Coefficient/quiz.py
@@ -0,0 +1,28 @@
+# Spearman's Rank Correlation Coefficient
+# Quiz: Given two n-element data sets, X and Y, calculate the value of
+# Spearman's rank correlation coefficient.
+
+# Define functions
+def rank(dt):
+    rank = {}
+    sort = sorted(dt)
+    for i in range(len(dt)):
+        for j in range(len(sort)):
+            if dt[i] == sort[j]:
+                rank[dt[i]] = j + 1
+    return rank
+
+def spearman(x, y, rx, ry, n):
+    pass
+
+# Set data
+n = int(float(input()))
+data_set_x = list(map(float, input().split()))
+data_set_y = list(map(float, input().split()))
+
+# Get rank
+rank_x = rank(data_set_x)
+rank_y = rank(data_set_y)
+
+# Gets the result and show on the screen
+print(round(spearman(data_set_x, data_set_y, rank_x, rank_y, n), 3))
