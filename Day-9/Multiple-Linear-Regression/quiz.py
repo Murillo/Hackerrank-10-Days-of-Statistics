6b18d5a0c60412b445058df0b0b0dc6b167d5b6e
Murillo <msgrubler@gmail.com>
2017-04-04 20:39:34 -0300

    Last day about Multiple Linear Regression

--- Day-9/Multiple-Linear-Regression/quiz.py
+++ Day-9/Multiple-Linear-Regression/quiz.py
@@ -3,11 +3,41 @@
 
 # Question: https://www.hackerrank.com/challenges/s10-multiple-linear-regression
 
+# Import library
 from sklearn import linear_model
-x = [[5, 7], [6, 6], [7, 4], [8, 5], [9, 6]]
-y = [10, 20, 60, 40, 50]
-lm = linear_model.LinearRegression()
-lm.fit(x, y)
-a = lm.intercept_
-b = lm.coef_
-print ("{}, {}, {}".format(a, b[0], b[1]))
+
+# Set data
+m, n = map(int, input().split())
+X, Y = [], []
+
+# Get the parameters X and Y for discovery the variables a and b
+for i in range(n):
+    x = [0]
+    elements = list(map(float, input().split()))
+    for j in range(len(elements)):
+        if j < m:
+            x.append(elements[j])
+        else:
+            Y.append(elements[j])
+    X.append(x)
+
+# Set the model LinearRegression
+model = linear_model.LinearRegression()
+model.fit(X, Y)
+a = model.intercept_
+b = model.coef_
+
+# Get the parameters X for discovery the Y
+q = int(input())
+new_X = []
+for i in range(q):
+    x = [0]
+    elements = list(map(float, input().split()))
+    for j in range(len(elements)):
+        x.append(elements[j])
+    new_X.append(x)
+
+# Gets the result and show on the screen
+result = model.predict(new_X)
+for i in range(len(result)):
+    print(round(result[i],2))
